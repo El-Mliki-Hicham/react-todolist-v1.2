68413fe07ef984e6ef55e271584ab9dcc1aad0f6
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-27 07:09:52 -0700

    todolist v1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+import Form from './components/Forms';
+import React from 'react';
 
-function App() {
+
+class App extends React.Component {
+
+  state={
+    data:[
+      {id:1,name:'hicham'},
+      {id:2,name:'amal'}
+    ]
+  }
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Table data={this.state.data} />
+    <br></br>
+    <Form  />
+  </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Forms.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Form extends React.Component{
+
+    render(){
+
+        return(
+        <div>
+           name <input></input>
+           <button>add new</button>
+        </div>
+        )
+    }
+}
+export default Form 
