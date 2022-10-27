4336173239ab2c1e8b7b8fa7468ff958a1b2d89a
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-27 08:57:45 -0700

    forms

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,12 +2,38 @@ import React from "react";
 
 class Form extends React.Component{
 
+state={
+    name:'',
+    id:""
+}
+handleSubmit=(e)=>{
+    e.preventDefault()
+    this.setState({
+       name: this.state.name,
+        id: Math.random()
+    })
+}
+
+ handleChnage=(e)=>{
+    this.setState({
+        name: e.target.value,
+         id: Math.random()
+     })
+    console.log(e.target.value)
+    console.log(this.state)
+}
     render(){
 
         return(
         <div>
-           name <input></input>
+
+
+            <h1>{this.state.name}</h1>
+            <h1>{this.state.id}</h1>
+            <form onSubmit={this.handleSubmit}>
+           name <input name="name"  onChange={this.handleChnage}></input>
            <button>add new</button>
+           </form>
         </div>
         )
     }
